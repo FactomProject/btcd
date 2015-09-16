66e5cff90b44ee9ec5ef85734c30b7fb3b8fed71
SirHalsell <sir.halsell@gmail.com>
2015-09-15 20:59:33 -0500

    update unit tests

--- btcjson/chainsvrresults_test.go
+++ btcjson/chainsvrresults_test.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcjson"
+	"github.com/FactomProject/btcd/btcjson"
 )
 
 // TestChainSvrCustomResults ensures any results that have custom marshalling
